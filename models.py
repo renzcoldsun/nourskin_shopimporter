cde7b2467180efd89e328ae4ca87b8867aad2c0c
Lawrence Salmingo <lawrence@salmingo.com>
2021-03-03 02:49:39 +0800

    adding payout_item.sql as base items

--- models.py
+++ models.py
@@ -3,22 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker
 from config import connection_string
-from woocommerce import API as wooo_api
 
 engine = create_engine(connection_string)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
-
-api_consumer_key = "ck_f65e9c5b696f93404da8cf79fdfd98fe5da293cc"
-api_consumer_secret = "cs_92a2c541f735b0ac90000875194b6a961321b462"
-
-wcapi = wooo_api(
-                url="http://localhost",
-                consumer_key = api_consumer_key,
-                consumer_secret = api_consumer_secret,
-                version = "wc/v3"
-                )
 
 class Item(Base):
     __tablename__ = 'payout_item'
@@ -29,11 +18,8 @@ class Item(Base):
     sale_price = Column(Float)
     dealer_price = Column(Float)
 
-for item in session.query(Item):
-    product_link = "products/" + str(item.id)
-    woo_item = wcapi.get(product_link)
-    if woo_item.status_code == 404:
-        print("INSERT")
-    else:
-        print("UPDATE")
-
+    def getItems(self):
+        items = [];
+        for item in session.query(Item):
+            items.append(item)
+        return items
